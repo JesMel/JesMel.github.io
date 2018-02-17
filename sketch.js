9854d3bc659ff463ad5dcb6342c12493f4cc83a5
JesMel <melissa.jesmin@gmail.com>
2018-02-17 16:18:23 -0600

    Hi

--- sketch.js
+++ sketch.js
@@ -4,5 +4,17 @@ createCanvas(600,400);
 
 
 function draw() {
+  //red background
+background(255,0,0);
+
+stroke(0,0,255);
+if(mouseX > 300) {
+  fill(0,255,0);
+}
+else {
+fill(0,0,255);
+}
+
+strokeWeight(5);
 ellipse(50,50,50,50);
 }
